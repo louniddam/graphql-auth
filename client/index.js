46e8092ebfe2911d7ac488d6f2f1c30213405013
Lou <louniddam@nipsi.io>
2021-06-01 16:10:29 +0200

    create new option in apollo client to bind cookies with request with a networkInterface

--- client/index.js
+++ client/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+import { Route, Router, hashHistory, IndexRoute } from 'react-router'
+
+import App from './components/App'
+
+const networkInterface = new createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+})
 
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 })
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
-        Auth Starter
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}/>
+      </Router>
     </ApolloProvider>
 
   );
