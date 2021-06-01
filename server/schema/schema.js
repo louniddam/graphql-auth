a75ab896c99b8e118063a96feaf7df6fa7ef543b
Lou <louniddam@nipsi.io>
2021-06-01 14:50:47 +0200

    setup the app
    create DB
    
    create query and mutations
    
    test with graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,11 @@
 const graphql = require('graphql');
+const mutation = require('./mutations');
 const { GraphQLSchema } = graphql;
 
 const RootQueryType = require('./types/root_query_type');
+// const UserType = require ('./types/user_type')
 
 module.exports = new GraphQLSchema({
-  query: RootQueryType
+  query: RootQueryType,
+  mutation
 });
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://root:rootroot@auth-graphql-shard-00-00.yqzgi.mongodb.net:27017,auth-graphql-shard-00-01.yqzgi.mongodb.net:27017,auth-graphql-shard-00-02.yqzgi.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-xd9wpj-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
